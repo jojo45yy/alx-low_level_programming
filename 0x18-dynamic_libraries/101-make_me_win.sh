a8b14eaf5b5ebcef54c3448c1a4e72c6135bbc02
jojo45yy <arrach20@gmail.com>
2023-12-12 11:23:13 -0800

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash 
-wget -P
+wget -P .. https://raw.githubusercontent.com/jojo45yy/alx-low_level_programming/master/0x18-dynamic_libraries/libcheat.so
+export LD_PRELOAD="$PWD/../libcheat.so"
